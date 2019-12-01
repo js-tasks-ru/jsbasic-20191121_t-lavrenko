e9773b7ba2cf7f2922cf9bc6813466c65d474309
Taras <lavrenko.taras@gmail.com>
2019-12-01 23:57:46 +0200

    2-module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,25 @@
  * @returns {Object}
  */
 function clone(obj) {
+  let newObj = {};
+  for (let key in obj) {
+    if (typeof obj[key] !== 'object') {
+      newObj[key] = obj[key];
+    }
+    else if (typeof obj[key] == 'object') {
+      newObj[key] = {};
+      for (let secondKey in obj[key]) {
+        if (typeof obj[key][secondKey] !== 'object') {
+          newObj[key][secondKey] = obj[key][secondKey];
+        }
+        else if (typeof obj[key][secondKey] == 'object') {
+          newObj[key][secondKey] = {};
+          for (let thirdKey in obj[key][secondKey]) {
+            newObj[key][secondKey][thirdKey] = obj[key][secondKey][thirdKey];
+          }
+        }
+      }
+    }
+  }
+  return newObj;
 }
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -4,4 +4,14 @@
  * @returns {Boolean}
  */
 function isEmpty(obj) {
+  let count = 0;
+  for (let key in obj) {
+    count += 1;
+  }
+  if (count == 0) {
+    return true;
+  }
+  if (count !== 0) {
+    return false;
+  }
 }
