d9e4f5d4a9dd5438434119c358a834b976e1be5a
Taras <lavrenko.taras@gmail.com>
2019-12-05 02:15:15 +0200

    3-module

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,4 +5,13 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+  let newData = '';
+  data.forEach((item) => {
+    if (item.age <= age) {
+      newData += item.name + ', ' + item.balance + '\n';
+    }  
+  }
+  );
+  let result = newData.slice(0, length - 1);
+  return result;
 }
