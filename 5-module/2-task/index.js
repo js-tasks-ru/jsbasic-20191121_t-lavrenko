845288d2be58ebf31b1d6bc133a2ae71b7d36c96
Taras <lavrenko.taras@gmail.com>
2019-12-22 00:37:53 +0200

    5-module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -25,5 +25,51 @@ function SortableTable(items) {
    * нужно выполнить сортировку (отсчет начинается от 0)
    * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
    */
-  this.sort = (column, desc = false) => {};
+  let head = document.createElement('thead');
+  let headRow = document.createElement('tr');
+  let tableHead = ['Name', 'Age', 'Salary', 'City'];
+  tableHead.forEach(function(item) {
+    let cell = document.createElement('td');
+    cell.innerHTML = item;
+    headRow.appendChild(cell);
+  });
+  head.appendChild(headRow);
+  this.el.appendChild(head);
+  let tableBody = document.createElement('tbody');
+  items.forEach(function(item) {
+    let tableRow = document.createElement('tr');
+    for (objItem in item) {
+      let bodyCell = document.createElement('td');
+      bodyCell.innerHTML = item[objItem];
+      tableRow.appendChild(bodyCell);
+      tableBody.appendChild(tableRow);
+    }
+  });
+  this.el.appendChild(tableBody);
+  this.sort = (column, desc = false) => {
+    let peopleArray = Array.from(tableBody.rows);
+    if (column === 0) {
+      let compare = function(a, b) {
+        console.log(a.cells[column].innerHTML > b.cells[column].innerHTML);
+        return a.cells[column].innerHTML > b.cells[column].innerHTML ? 1 : -1;
+      };
+      peopleArray.sort(compare);
+      if (desc === true) {
+        peopleArray.reverse();
+      }
+      tableBody.append(...peopleArray);
+    }
+    if (column === 2) {
+      let compare = function(a, b) {
+        return a.cells[column].innerHTML - b.cells[column].innerHTML;
+      };
+      peopleArray.sort(compare);
+      if (desc === true) {
+        peopleArray.reverse();
+      }
+      tableBody.append(...peopleArray);
+    }
+  };
 }
+
+
